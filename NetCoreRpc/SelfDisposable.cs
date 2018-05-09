2bfac8eb0c65715495baaedde660d1595d5b2f70
yjq <425527169@qq.com>
2018-05-09 09:55:02 +0800

    添加遗漏的文件

---
+++ NetCoreRpc/SelfDisposable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NetCoreRpc
+{
+    /// <summary>
+    /// 类名：SelfDisposable.cs
+    /// 类功能描述：
+    /// 创建标识：yjq 2018/5/8 10:11:03
+    /// </summary>
+    public class SelfDisposable : IDisposable
+    {
+        private bool _isDisposed;
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void DisposeCode()
+        {
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed && disposing)
+            {
+                DisposeCode();
+            }
+            _isDisposed = true;
+        }
+
+        ~SelfDisposable()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ NetCoreRpc/Serializing/RpcSerializer/SerializerInputStream.cs
@@ -0,0 +1,78 @@
+﻿using NetCoreRpc.Utils;
+using System;
+using System.IO;
+
+namespace NetCoreRpc.Serializing.RpcSerializer
+{
+    /// <summary>
+    /// 类名：SerializerInputStream.cs
+    /// 类功能描述：
+    /// 创建标识：yjq 2018/5/8 10:11:51
+    /// </summary>
+    public class SerializerInputStream : SelfDisposable
+    {
+        public SerializerInputStream()
+        {
+            CurrentStream = new MemoryStream();
+        }
+
+        public MemoryStream CurrentStream { get; }
+
+        public int Length
+        {
+            get
+            {
+                return (int)CurrentStream.Length;
+            }
+        }
+
+        public void Write(byte value)
+        {
+            CurrentStream.WriteByte(value);
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            CurrentStream.Write(buffer, offset, count);
+        }
+
+        public void Write(byte[] buffer)
+        {
+            CurrentStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public void Write(int value)
+        {
+            Write(BitConverter.GetBytes(value), 0, 4);
+        }
+
+        public void UpdateCurrentLength(int value, int offset)
+        {
+            CurrentStream.Position = offset;
+            Write(BitConverter.GetBytes(value), 0, 4);
+            CurrentStream.Position = CurrentStream.Length;
+        }
+
+        public void Write(long value)
+        {
+            Write(BitConverter.GetBytes(value), 0, 8);
+        }
+
+        public void Write(string value)
+        {
+            ByteUtil.EncodeString(value, out byte[] lengthBytes, out byte[] dataBytes);
+            Write(lengthBytes);
+            Write(dataBytes);
+        }
+
+        public byte[] ToBytes()
+        {
+            return CurrentStream.ToArray();
+        }
+
+        protected override void DisposeCode()
+        {
+            CurrentStream.Dispose();
+        }
+    }
+}
