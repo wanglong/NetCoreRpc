2696163a400ea06921908bb827030acf0e3d2c62
yjq <425527169@qq.com>
2018-05-15 15:26:55 +0800

    111

---
+++ NRpc.AdminManage/Dtos/RpcMonitorMethodRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NRpc.AdminManage.Dtos
+{
+    public class RpcMonitorMethodRequestDto
+    {
+        public string RequestTypeName { get; set; }
+
+        public string RequestMethodName { get; set; }
+
+        public int TotalExcuteCount { get; set; }
+
+        public int ErrorCount { get; set; }
+    }
+}
